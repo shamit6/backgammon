23cbc9a9907865f4b15fd7a0aba83756633db9db
shamit6 <amishtalev@gmail.com>
2017-05-20 21:41:57 +0300

    6-6

--- dev/app/components/DicingViewer.jsx
+++ dev/app/components/DicingViewer.jsx
@@ -50,8 +50,8 @@ const randomize = (maxNumber) => {
   }
 
   onDicing() {
-    const dice1Number = 6;//randomize(6);
-    const dice2Number = 6;//randomize(6);
+    const dice1Number = randomize(6);
+    const dice2Number = randomize(6);
     this.dicingFire.dice1(dice1Number);
     this.dicingFire.dice2(dice2Number);
     //this.setState({dice1:dice1Number, dice2:dice2Number});
